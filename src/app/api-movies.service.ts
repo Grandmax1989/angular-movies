926cfb57f170dcf08dbd7c8308f7ed3a04aa837d
jupegarnica <jupegarnica@gmail.com>
2019-05-09 13:07:35 +0200

    api service typed

--- src/app/api-movies.service.ts
+++ src/app/api-movies.service.ts
@@ -7,12 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class ApiMoviesService {
   urlBase = `https://api.themoviedb.org/3/`;
   key = '323112ea2281b9eb70f319f4df422c6b';
-  generateUrl = (_type) => this.urlBase + `movie/${_type}?api_key=` + this.key;
+  generateUrl = (_type: string) => this.urlBase + `movie/${_type}?api_key=` + this.key;
 
   constructor(private http: HttpClient){
   }
 
-  getMovies(type){
+  getMovies(type: string){
 
     return this.http.get(this.generateUrl(type))
   }
